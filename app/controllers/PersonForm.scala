11d583a2a45d98f518b57861468cfddc68fc0016
yamato3310 <yamato3310.tuna@gmail.com>
2019-07-17 11:12:47 +0900

    3.1

---
+++ app/controllers/PersonForm.scala
@@ -0,0 +1,16 @@
+pacage package controllers
+ 
+object PersonForm {
+    import play.api.data._
+    import play.api.data.Forms._
+
+    case class Data(name: String, mail: String, tel: String)
+
+    val form = Form(
+        mapping(
+            "name" -> text,
+            "mail" -> text,
+            "tel" -> text
+        )(Data.apply)(Data.unapply)
+    )
+}
